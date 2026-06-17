4a6eecd276594bdeabad1c2cbda568176b85219e
Tersom <ibnua0291@gmail.com>
2026-06-17 12:47:55 +0700

    Chapter 5: Finally, Some UI To Play With

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -10,7 +10,9 @@
     """,
     # data files always loaded at installation
     'data': [
-        # 'views/mymodule_view.xml',
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
     ],
     # data files containing optionally loaded demonstration data
     'demo': [
