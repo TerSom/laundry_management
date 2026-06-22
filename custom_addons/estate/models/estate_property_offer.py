44662b57cc8bbacadb4cbe4c1c120133f8aa3881
Tersom <ibnua0291@gmail.com>
2026-06-22 12:07:44 +0700

    Chapter 9: Ready For Some Action?

--- custom_addons/estate/models/estate_property_offer.py
+++ custom_addons/estate/models/estate_property_offer.py
@@ -8,7 +8,7 @@ class EstatePropertyOffer(models.Model):
 
     price = fields.Float()
     status = fields.Selection([('accepted', 'Accepted'), ('refused', 'Refused')], copy=False , readonly=True)
-    partner_id = fields.Many2one('res.partner', string='Partner', required=True, readonly=True``)
+    partner_id = fields.Many2one('res.partner', string='Partner', required=True, readonly=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
     validity = fields.Integer(string='Validity (days)', default=7)
     date_deadline = fields.Date(compute='_compute_date_deadline', inverse='_inverse_date_deadline')
