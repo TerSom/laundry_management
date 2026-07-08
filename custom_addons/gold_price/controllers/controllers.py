34437f86d3390a8e159062395c3f0e5c54998b61
Tersom <ibnua0291@gmail.com>
2026-07-08 20:40:41 +0700

    api

--- custom_addons/gold_price/controllers/controllers.py
+++ custom_addons/gold_price/controllers/controllers.py
@@ -7,6 +7,7 @@ from odoo.http import request
 
 class GoldPriceController(http.Controller):
 
+# get api
     @http.route(
         ['/api/gold_price_get/'],
         type='http',
@@ -42,6 +43,7 @@ class GoldPriceController(http.Controller):
                 status=500
             )
 
+# post api
     @http.route(
         ['/api/local_gold_price/'],
         type='http',
@@ -50,7 +52,6 @@ class GoldPriceController(http.Controller):
         csrf=False
     )
     def local_gold_price(self, **params):
-
         url = "http://localhost:8018/api/gold_price_get/"
 
         try:
@@ -87,3 +88,82 @@ class GoldPriceController(http.Controller):
                 content_type="application/json",
                 status=500
             )
+
+# put api 
+    @http.route(
+    ['/api/gold_price_put/'],
+    type='http',
+    auth='public',
+    methods=['PUT'],
+    csrf=False
+    )
+    def gold_price_put(self, **params):
+        data = json.loads(request.httprequest.data)
+
+        gold = request.env["gold.price"].sudo().search([
+            ("symbol", "=", data.get("symbol"))
+        ], limit=1)
+
+        if not gold:
+            return http.Response(
+                json.dumps({
+                    "status": 404,
+                    "message": "Data tidak ditemukan"
+                }),
+                content_type="application/json",
+                status=404
+            )
+
+        gold.write({
+            "name": data.get("name"),
+            "price": data.get("price"),
+            "currency": data.get("currency"),
+            "updated_at": data.get("updated_at"),
+        })
+
+        return http.Response(
+            json.dumps({
+                "status": 200,
+                "message": "Data berhasil diupdate",
+                "data" : data
+            }),
+            content_type="application/json",
+            status=200
+        )
+
+# delete api
+    @http.route(
+    ['/api/gold_price_delete/'],
+    type='http',
+    auth='public',
+    methods=['DELETE'],
+    csrf=False
+    )
+    def gold_price_delete(self, **params):
+
+        data = json.loads(request.httprequest.data)
+
+        gold = request.env["gold.price"].sudo().search([
+            ("name", "=", data.get("name"))
+        ], limit=1)
+
+        if not gold:
+            return http.Response(
+                json.dumps({
+                    "status": 404,
+                    "message": "Data tidak ditemukan"
+                }),
+                content_type="application/json",
+                status=404
+            )
+
+        gold.unlink()
+
+        return http.Response(
+            json.dumps({
+                "status": 200,
+                "message": "Data berhasil dihapus"
+            }),
+            content_type="application/json",
+            status=200
+        )
